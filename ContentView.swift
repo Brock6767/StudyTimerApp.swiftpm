60f34249e6840a5decf909bcbd4f38b64223a626
Kayan Safarpour <Ksafarpour9342@stu.d214.org>
2026-05-07 13:47:19 -0500

    d

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, sus!")
+            Text("Hello, diddy!")
         }
     }
 }
