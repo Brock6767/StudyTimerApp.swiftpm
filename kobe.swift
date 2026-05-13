232e8f9cbeefcbb1b57af9b78121f18c3f618c9d
Kayan Safarpour <Ksafarpour9342@stu.d214.org>
2026-05-13 13:51:10 -0500

    67676

--- kobe.swift
+++ kobe.swift
@@ -1,1 +1,31 @@
+import SwiftUI
 
+struct StreakView: View {
+
+    @State var streak = 0
+
+    var body: some View {
+
+        VStack(spacing: 30) {
+
+            Text("Study Streak")
+                .font(.largeTitle)
+
+            Text("🔥 \(streak) Days")
+                .font(.system(size: 45))
+
+            Button("I Studied Today") {
+                streak += 1
+            }
+            .padding()
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+
+            Button("Reset Streak") {
+                streak = 0
+            }
+            .padding()
+        }
+    }
+}
